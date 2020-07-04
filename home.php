a7d952ba64c2d15a118dd26d46998034c501dab5
nicholas <nicholasallenmaurer@gmail.com>
2020-07-04 19:45:15 +1000

    working on home

---
+++ home.php
@@ -0,0 +1,71 @@
+<?php
+/* Template Name: Home */
+get_header();
+?>
+
+<section class="hero">
+    <img src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png" alt="Resevoir Doctors">
+    <div class='container'>
+        <div class='row medium-unstack hero__row'>
+            <div class="hero__content columns">
+                <h1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                    labore et dolore magna aliquyam erat, sed</h1>
+            </div>
+        </div> <!-- .row -->
+    </div> <!-- .container -->
+</section>
+<section class="staff">
+    <div class='container'>
+        <div class="row">
+            <div class="columns text-right staff__heading">Staff</div>
+        </div>
+        <div class="row staff__row">
+            <div class="columns staff_content">
+                <div class="row">
+                    <div class="column">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                        takimata
+                        sanctus
+                        est Lorem ipsum dolor sit amet.
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="column small-1"><img
+                            src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Email-icon.png"
+                            alt="Email"></div>
+                    <div class="column small-1"><img
+                            src="http://4.bp.blogspot.com/-FxsLDYb9X4c/UCT06y4PDII/AAAAAAAAIig/4d9h6_vpuaM/s1600/LinkedIn.png"
+                            alt="LinkedIn"></div>
+                </div>
+            </div>
+            <div class="columns  staff__list">
+                <ul class="staff__ul">
+                    <li>
+                        <div class="staff_list_heading"><strong>Heading</strong></div>
+                        <div class="staff_list_name">Name</div>
+                    </li>
+                    <li>
+                        <div class="staff_list_heading"><strong>Heading</strong></div>
+                        <div class="staff_list_name">Name</div>
+                    </li>
+                    <li>
+                        <div class="staff_list_heading"><strong>Heading</strong></div>
+                        <div class="staff_list_name">Name</div>
+                    </li>
+                    <li>
+                        <div class="staff_list_heading"><strong>Heading</strong></div>
+                        <div class="staff_list_name">Name</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<?php
+get_footer();
