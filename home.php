52fcf4e7f6b3dcc03e9d8172facd2932d48ee29a
nicholas <nicholasallenmaurer@gmail.com>
2020-07-11 18:51:19 +1000

    working on home

--- home.php
+++ home.php
@@ -1,5 +1,12 @@
 <?php
 /* Template Name: Home */
+$staffText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+takimata
+sanctus
+est Lorem ipsum dolor sit amet.";
 get_header();
 ?>
 
@@ -15,52 +22,90 @@ get_header();
     </div> <!-- .container -->
 </section>
 <section class="staff">
-    <div class='container'>
+    <div class='container staff_container'>
         <div class="row">
             <div class="columns text-right staff__heading">Staff</div>
         </div>
         <div class="row staff__row">
-            <div class="columns staff_content">
-                <div class="row">
-                    <div class="column">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-
-                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                        takimata
-                        sanctus
-                        est Lorem ipsum dolor sit amet.
+            <div class="row staff__tabs_container">
+                <div class="columns staff__tabs_title_container">
+                    <ul class="vertical tabs staff__tabs_override" data-tabs id="example-tabs">
+                        <li class="tabs-title is-active"><a href="#panel1v" aria-selected="true">
+                                <div>Brad Sherman</div>
+                                <div>Director</div>
+                            </a></li>
+                        <li class="tabs-title"><a href="#panel2v">
+                                <div>Brad Sherman</div>
+                                <div>Director</div>
+                            </a></li>
+                        <li class="tabs-title"><a href="#panel3v">
+                                <div>Brad Sherman</div>
+                                <div>Director</div>
+                            </a></li>
+                        <li class="tabs-title"><a href="#panel4v">
+                                <div>Brad Sherman</div>
+                                <div>Director</div>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="columns staff__tabs_content_container">
+                    <div class="tabs-content staff__tabs_content_override" data-tabs-content="example-tabs">
+                        <div class="tabs-panel is-active" id="panel1v">
+                            <div class="row">
+                                <p><?php echo $staffText ?></p>
+                            </div>
+                            <div class="row">
+                                <div class="columns">
+                                    <div class="row">
+                                        <div class="staff__icon"></div>
+                                        <div class="staff__icon"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tabs-panel" id="panel2v">
+                            <div class="row">
+                                <p><?php echo $staffText ?></p>
+                            </div>
+                            <div class="row">
+                                <div class="columns">
+                                    <div class="row">
+                                        <div class="staff__icon"></div>
+                                        <div class="staff__icon"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tabs-panel" id="panel3v">
+                            <div class="row">
+                                <p><?php echo $staffText ?></p>
+                            </div>
+                            <div class="row">
+                                <div class="columns">
+                                    <div class="row">
+                                        <div class="staff__icon"></div>
+                                        <div class="staff__icon"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tabs-panel" id="panel4v">
+                            <div class="row">
+                                <p><?php echo $staffText ?></p>
+                            </div>
+                            <div class="row">
+                                <div class="columns">
+                                    <div class="row">
+                                        <div class="staff__icon"></div>
+                                        <div class="staff__icon"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="column small-1"><img
-                            src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Email-icon.png"
-                            alt="Email"></div>
-                    <div class="column small-1"><img
-                            src="http://4.bp.blogspot.com/-FxsLDYb9X4c/UCT06y4PDII/AAAAAAAAIig/4d9h6_vpuaM/s1600/LinkedIn.png"
-                            alt="LinkedIn"></div>
-                </div>
             </div>
-            <div class="columns  staff__list">
-                <ul class="staff__ul">
-                    <li>
-                        <div class="staff_list_heading"><strong>Heading</strong></div>
-                        <div class="staff_list_name">Name</div>
-                    </li>
-                    <li>
-                        <div class="staff_list_heading"><strong>Heading</strong></div>
-                        <div class="staff_list_name">Name</div>
-                    </li>
-                    <li>
-                        <div class="staff_list_heading"><strong>Heading</strong></div>
-                        <div class="staff_list_name">Name</div>
-                    </li>
-                    <li>
-                        <div class="staff_list_heading"><strong>Heading</strong></div>
-                        <div class="staff_list_name">Name</div>
-                    </li>
-                </ul>
-            </div>
+
         </div>
     </div>
 
